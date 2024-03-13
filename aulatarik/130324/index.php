1ea3c8c6773f451fff76b176aae937c9b8bfc5ed
pabloalves <sengineerpablo@gmail.com>
2024-03-13 20:47:34 -0300

    classe | objeto | atributo funcionarios

---
+++ aulatarik/130324/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Menu</h1>
+
+    <ul>
+        <li><a href="funcionario.php">Ver Funcionários</a></li>
+        <li><a href="inserirfuncionario.php">Inserir Funcionário</a></li>
+        <li><a href="alterarfuncionario.php">Alterar Funcionário</a></li>
+        <li><a href="excluirfuncionario.php">Remover Funcionários</a></li>
+    </ul>
+
+
+</body>
+</html>
