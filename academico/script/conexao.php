bd0031de5bff450c4fc02193981e485543275d71
Pablo Alves <sengineerpablo@gmail.com>
2024-07-04 13:41:23 -0300

    login page bootstrap

--- academico/script/conexao.php
+++ academico/script/conexao.php
@@ -1,5 +1,5 @@
 <?php  
-$conexao = mysqli_connect("localhost", "root", "", "academico", "3307");
+$conexao = mysqli_connect("localhost", "root", "", "academico");
 
     if (!$conexao) {
         die ('Não foi possível conectar ao banco de dados'.mysqli_connect_error());
