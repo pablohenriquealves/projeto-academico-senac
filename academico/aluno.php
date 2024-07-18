b8783dbdebe225c6f1b5f8c760371082f1ae7e85
Pablo Alves <sengineerpablo@gmail.com>
2024-07-18 02:26:30 -0300

    finalizado form aluno.php

--- academico/aluno.php
+++ academico/aluno.php
@@ -7,31 +7,16 @@
 <body id="aluno">
 
 <div class="container-fluid">
+<?php include 'nav.php' ?>
+
     <div class="row">
         <?php include 'aside.php' ?>
         
         <main class="col-md-9">
-            <div>    <h1>Cadastro dos Alunos</h1>    </div>
+            <div class="text-center">    <h1>Cadastro dos Alunos</h1>    </div>
             <div class="card col-md-7">
                 <div class="card-body">
-                    <form action="" method="post">
-                        <label for="matricula">Matrícula</label>
-                        <input type="text" name="matricula" id="matricula" placeholder="Digite a matrícula">
-                        <label for="nome">Nome</label>
-                        <input type="text" name="nome" id="nome" placeholder="Digite o nome do aluno">
-                        <label for="datanascimento">Data do Nascimento</label>
-                        <input type="date" name="datanascimento" id="datanascimento">
-                        <label for="genero">Gênero</label>
-                        <input type="radio" name="genero" id="genero" value="Masculino">Masculino
-                        <input type="radio" name="genero" id="genero" value="Feminino">Feminino
-                        <label for="nota1">Primeira nota</label>
-                        <input type="number" name="nota1" id="nota1" value="nota1" min="0" max="10">
-                        <label for="nota2">Segunda nota</label>
-                        <input type="number" name="nota2" id="nota2" value="nota2" min="0" max="10">
-                        <input type="button" value="Calcular Média" ONCLICK="resultado(this.form)">              
-                        
-                        
-                        <form class="row g-3">
+                        <form class="row g-3" action="" method="post">
                         <div class="col-md-6">
                             <label for="matricula" class="form-label">Matrícula</label>
                             <input type="text" class="form-control" id="matricula" placeholder="Digite a matrícula">
@@ -41,52 +26,33 @@
                             <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite o nome do aluno">
                         </div>
                         <div class="col-md-6">
-                        <label for="datanascimento">Data do Nascimento</label>
+                        <label class="form-check-label" for="datanascimento">Data do Nascimento</label>
                         <input type="date" class="form-control" name="datanascimento" id="datanascimento">
                         </div>
-                        <div class="col-md-6">
-                        <div class="form-check">
                         <label for="genero">Gênero</label>
+                        <div class="form-check">
                         <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                         <label class="form-check-label" for="flexRadioDefault1">
                             Masculino
                         </label>
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                        <label class="form-check-label" for="flexRadioDefault1">
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                        <label class="form-check-label" for="flexRadioDefault2">
                             Feminino
                         </label>
                         </div>
+                        <div class="col-md-3">
+                        <label class="form-check-label" for="nota1">Primeira nota</label>
+                        <input class="form-control" type="number" name="nota1" id="nota1" value="nota1" min="0" max="10">
                         </div>
-                        <div class="col-md-6">
-                        <label for="nota1">Primeira nota</label>
-                        <input type="number" name="nota1" id="nota1" value="nota1" min="0" max="10">
+                        <div class="col-md-3">
+                        <label class="form-check-label" for="nota2">Segunda nota</label>
+                        <input class="form-control" type="number" name="nota2" id="nota2" value="nota2" min="0" max="10">
+                        <input type="button" class="btn btn-warning mt-1" value="Calcular Média" ONCLICK="resultado(this.form)">
                         </div>
-                        <div class="col-md-6">
-                        <label for="nota2">Segunda nota</label>
-                        <input type="number" name="nota2" id="nota2" value="nota2" min="0" max="10">
-                        <input type="button" value="Calcular Média" ONCLICK="resultado(this.form)">
-                        </div>
-                        <div class="col-md-4">
-                            <label for="inputState" class="form-label">State</label>
-                            <select id="inputState" class="form-select">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                            </select>
-                        </div>
-                        <div class="col-md-2">
-                            <label for="inputZip" class="form-label">Zip</label>
-                            <input type="text" class="form-control" id="inputZip">
-                        </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck">
-                            <label class="form-check-label" for="gridCheck">
-                                Check me out
-                            </label>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        <div class="col-12 mt-1">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
                         </div>
                         </form>
 
--- academico/dashboard.php
+++ academico/dashboard.php
@@ -21,52 +21,7 @@ if (!isset($_SESSION['usuario'])) {
 <body id="inicio">
 
 <div class="container-fluid">
-    <header class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4 border-bottom">
-      <!-- Logo e título -->
-      <a href="dashboard.php" class="d-flex mb-3 mb-md-0 text-decoration-none link-body-emphasis">
-        <img src="imagens/logosenacv2.png" alt="logo senac" class="logo">
-      </a>
-
-      <div class="fs-4 ms-2 text-center"><h1>Controle Acadêmico</h1></div>
-
-      <!-- Navegação e formulário de pesquisa -->
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex ms-2" role="search">
-              <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-              <button class="btn btn-outline-primary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </header>
+<?php include 'nav.php' ?>
 
 <div class="row">
             <?php include 'aside.php' ?>
