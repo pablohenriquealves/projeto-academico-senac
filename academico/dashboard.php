48e3574bef4173b9bd70bd731e114f910f300d23
Pablo Alves <sengineerpablo@gmail.com>
2024-07-22 23:09:48 -0300

    alteração layout dashboard

--- academico/dashboard.php
+++ academico/dashboard.php
@@ -21,15 +21,17 @@ if (!isset($_SESSION['usuario'])) {
 <div class="container-fluid">
 <?php include 'nav.php' ?>
 
-    <div class="row">
-        <?php include 'aside.php' ?>
-        
-        <main class="col-md-9">
-    <div><h2><p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p></h2></div>
-                <div class="card col-md-7">
-                <div class="card-body">
-                  <p>Aqui você poderá consultar <a href="notas.php" alt="notas da turma" target="_self">notas</a>.</p>
-                  <p>E também a <i><a href="frequencia.php" alt="frequencia dos alunos">frequência</a></i>   dos alunos.</p>
+<div class="row">
+  <?php include 'aside.php' ?>
+  
+  <main class="col-md-9 d-flex flex-column align-items-center">
+    <div class="text-center mb-3 w-100"><h2><p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p></h2></div>
+    <div class="card col-md-8">
+            <div class="card-body">
+              <div class="d-grid gap-2">
+            <a href="notas.php" class="btn btn-outline-secondary btn-lg">Aqui você poderá consultar notas</a>
+            <a href="notas.php" class="btn btn-outline-secondary btn-lg">E também a frequência dos alunos</a>
+            </div>
                   </div>
                 </div>
             </main>
