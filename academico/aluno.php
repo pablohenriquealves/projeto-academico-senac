db13d35cf383ed95d1165d9af4395e61bc2606c5
Pablo Alves <sengineerpablo@gmail.com>
2024-07-23 21:37:49 -0300

    alterando design dashboard

--- academico/aluno.php
+++ academico/aluno.php
@@ -16,8 +16,8 @@
             <div class="card col-md-8">
                 <div class="card-body">
                     <div class="d-grid gap-2">
-                    <a href="cadastroaluno.php" class="btn btn-outline-secondary btn-lg">Cadastrar Aluno</a>
-                    <a href="atualizaraluno.php" class="btn btn-outline-secondary btn-lg">Atualizar Aluno</a>
+                    <a href="cadastroaluno.php" class="btn btn-outline-secondary btn-lg">Cadastrar aluno</a>
+                    <a href="atualizaraluno.php" class="btn btn-outline-secondary btn-lg">Atualizar aluno</a>
                 </div>
                   </div>
                 </div>
--- academico/dashboard.php
+++ academico/dashboard.php
@@ -32,42 +32,79 @@ if (!isset($_SESSION['usuario'])) {
             <a href="notas.php" class="btn btn-outline-secondary btn-lg">Aqui você poderá consultar notas</a>
             <a href="notas.php" class="btn btn-outline-secondary btn-lg">E também a frequência dos alunos</a>
             </div> -->
-            <div class="row mb-2">
+    <div class="row mb-2">
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary-emphasis"></strong>
-          <h3 class="mb-0">Aqui você poderá consultar notas</h3>
-          <div class="mb-1 text-body-secondary"></div>
-          <p class="card-text mb-auto"></p>
-          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-            Consultar
+          <!-- <strong class="d-inline-block mb-2 text-primary-emphasis"></strong> -->
+          <h3 class="mb-0">Aqui você poderá atribuir notas</h3>
+          <!-- <div class="mb-1 text-body-secondary"></div> -->
+          <!-- <p class="card-text mb-auto"></p> -->
+          <a href="notas.php" class="icon-link gap-1 icon-link-hover stretched-link">
+            Atribuir
             <svg class="bi"><use xlink:href="#chevron-right"/></svg>
           </a>
         </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="fa-solid fa-pen-to-square" width="50" height="50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        <div class="col-auto d-none d-lg-flex align-items-center justify-content-center me-2">
+        <svg class="fa-solid fa-pen-to-square" width="50" height="50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
         </div>
       </div>
     </div>
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success-emphasis"></strong>
-          <h3 class="mb-0">E também a frequência dos alunos</h3>
-          <div class="mb-1 text-body-secondary"></div>
-          <p class="mb-auto"></p>
+          <!-- <strong class="d-inline-block mb-2 text-success-emphasis"></strong> -->
+          <h3 class="mb-0">Matricular aluno em um curso</h3>
+          <!-- <div class="mb-1 text-body-secondary"></div> -->
+          <!-- <p class="mb-auto"></p> -->
+          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+            Matricular
+            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-flex align-items-center justify-content-center me-2">
+          <svg class="fa-solid fa-calendar-check" width="50" height="50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <!-- <strong class="d-inline-block mb-2 text-primary-emphasis"></strong> -->
+          <h3 class="mb-0">Consultar disciplinas</h3>
+          <!-- <div class="mb-1 text-body-secondary"></div> -->
+          <!-- <p class="card-text mb-auto"></p> -->
+          <a href="notas.php" class="icon-link gap-1 icon-link-hover stretched-link">
+            Consultar
+            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-flex align-items-center justify-content-center me-2">
+        <svg class="fa-solid fa-pen-to-square" width="50" height="50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <!-- <strong class="d-inline-block mb-2 text-success-emphasis"></strong> -->
+          <h3 class="mb-0">E também a frequência do aluno</h3>
+          <!-- <div class="mb-1 text-body-secondary"></div> -->
+          <!-- <p class="mb-auto"></p> -->
           <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
             Frequência
             <svg class="bi"><use xlink:href="#chevron-right"/></svg>
           </a>
         </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        <div class="col-auto d-none d-lg-flex align-items-center justify-content-center me-2">
+          <svg class="fa-solid fa-calendar-check" width="50" height="50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
         </div>
       </div>
     </div>
-  </div>
+  </div> 
                   </div>
                 </div>
             </main>
